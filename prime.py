d6890edc0b3f5fe12be602a60558b106ff51b323
karthibaskar <47408860+karthibaskar@users.noreply.github.com>
2019-02-08 12:23:25 +0530

    Update prime.py

--- prime.py
+++ prime.py
@@ -1,17 +1,17 @@
-numb = 11
+numbr = 11
   
 
-if numb> 1: 
+if numbr> 1: 
       
-   # Iterate from 2 to n / 2  
-   for i in range(2, numb//2): 
+   
+   for i in range(2, numbr//2): 
          
   
-       if (numb % i) == 0: 
-           print(numb, "is not a prime number") 
+       if (numbr % i) == 0: 
+           print(numbr, "is not a prime number") 
            break
    else: 
-       print(numb, "is a prime number") 
+       print(numbr, "is a prime number") 
   
 else: 
    print(numb, "is not a prime number") 
